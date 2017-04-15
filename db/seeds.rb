e73b693ea5bc30d2ee97700aeb0a5646d8b539a3
James1988sp <gianmariacarrodano@libero.it>
2017-04-15 13:24:18 +0100

    DisplySeedOk

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,73 @@
 end
 
 [{
-  name: "The Beatles",
-  photo: "http://www.billboard.com/files/styles/article_main_image/public/media/the-beatles-circa-1966-650-430.jpg",
-  weight: 70,
-  skills: "Very fast",
-  speed: 65
+  name: "Gianmaria",
+  photo: "https://www.facebook.com/photo.php?fbid=10208416550525259&set=a.1476974658272.61668.1650906924&type=3&theater",
+  nationality: "Italian",
+  email: "gianmariacarrodano@libero.it",
+  password: "dsquared",
+  password_confirmation: "dsquared"
+},{
+  name: "Guv",
+  photo: "https://www.facebook.com/photo.php?fbid=10154567110826203&set=a.470372636202.275202.657506202&type=3&theater",
+  nationality: "English",
+  email: "guv@guv",
+  password: "gangsta",
+  password_confirmation: "gangsta"
+},{
+  name: "DonMarko",
+  photo: "https://www.facebook.com/photo.php?fbid=10153598594281159&set=a.458815111158.243626.503321158&type=3&theater",
+  nationality: "English",
+  email: "mark@mark",
+  password: "bello",
+  password_confirmation: "bello"
+},{
+  name: "ruthlessammo",
+  photo: "https://www.facebook.com/photo.php?fbid=10151911109181142&set=a.468960126141.268832.640086141&type=3&theater",
+  nationality: "English",
+  email: "will@will",
+  password: "daytona",
+  password_confirmation: "daytona"
+},{
+  name: "Giacomo",
+  photo: "https://www.facebook.com/giacobrunetti",
+  nationality: "Italian",
+  email: "giacomo@giacomo",
+  password: "Milano",
+  password_confirmation: "Milano"
 }].each do |user|
   User.create!(user)
+end
+
+[{
+  name: "DJI Mavic Pro N1265  ",
+  photo: "http://www.dronionline.net/new/wp-content/uploads/2016/10/dji-mavic-pro-fly-more-combo-dji-1-1.jpg",
+  weight: 30,
+  skills: "Very fast dude",
+  speed: 60,
+  user_id: 1
+}, {
+  name: "DJI Phantom 4",
+  photo: "https://asset1.djicdn.com/images/360/phantom-4/draggable_360_v2/0_0.png",
+  weight: 44,
+  skills: "Super dynamic drone",
+  speed: 65,
+  user_id: 2
+}].each do |drone|
+  Drone.create!(drone)
+end
+
+
+[{
+  name: "Jungle of discord",
+  location: "Liverpool",
+  date: Date.new(2017, 9, 29),
+  time: 18.00,
+}, {
+  name: "Royal Rumble",
+  location: "London",
+  date: Date.new(2017, 7, 10),
+  time: 18.00,
+}].each do |race|
+  Race.create!(race)
 end
